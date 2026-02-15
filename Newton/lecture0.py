414f2e38cea7e8c9f86f5fba8efb83069d8c1959
NewtonOduro <odurocollins345@gmail.com>
2026-02-15 14:36:13 +0000

    Newton Oduro Collins: tutorial

--- Newton/lecture0.py
+++ Newton/lecture0.py
@@ -1,1 +1,9 @@
-print("heelo")
+print("heelo world")
+
+name = input("What is your name:")
+print(f"Hello, {name} "+" 12 ")
+
+# removing spaces with the strip() function
+name = input("Enter your name:")
+name = name.strip().capitalize()
+print("Hello")
