98c950971fdd7239729ae6e6dd3762420fdbbbe0
Estherriches <estherriches286@gmail.com>
2026-02-25 02:14:19 +0000

    Esther: class lesson

--- esther_riches/lecture0/leacture4.py
+++ esther_riches/lecture0/leacture4.py
@@ -1,15 +1,16 @@
 # from random import randint,choices
 # print(randint(10,50))
 # print(choice(["banana","apple"]))
-import  random
+import random
 magic=random.randint(6,13)
 print(magic)
+
+
 
 import sys
 if sys.argv[1:]:
     
     print("module")
-
     
 else:
   
