12e43ac0ce5ff3ec59dfd66a9ce07cfb0e4dca02
Mcqueen <owususarah442@gmail.com>
2026-02-26 22:30:48 +0000

    Assignment 2 and 3

--- Sandra/Assignment.py
+++ Sandra/Assignment.py
@@ -1,31 +1,31 @@
 #creating a parity function to determine if a number is even or odd
 
-#def parity(num):
-#    if num% 2 == 0:
-#        return "even"
-#    else:
-#        return "odd"
+def parity(num):
+    if num% 2 == 0:
+        return "even"
+    else:
+        return "odd"
     
-#num = int(input("Enter a number: "))
-#print(parity(num))    
+num = int(input("Enter a number: "))
+print(parity(num))    
 
 
 #Using try and except function to pass an error.
 
-#def parity(num):
+def parity(num):
 
-#    if num% 2 == 0:
-#        return "even"
-#    else:
-#        return "odd"
-#while True:    
-#    try:
-#        user_input = int(input("Enter num: "))
-#        print(parity(user_input))
-#        break
+    if num% 2 == 0:
+        return "even"
+    else:
+        return "odd"
+while True:    
+    try:
+        user_input = int(input("Enter num: "))
+        print(parity(user_input))
+        break
     
-#    except(ValueError,TypeError):
-#        print("Invalid input") 
+    except(ValueError,TypeError):
+        print("Invalid input") 
      
 
 
