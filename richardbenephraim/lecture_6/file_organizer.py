700d7bac23c8290c8e283ff10f7451b48dfdb6e8
LocalCode-Tech <rbephraim95@gmail.com>
2026-02-28 17:33:12 +0000

    Richaard Ben Ephraim: File Organiser

--- richardbenephraim/lecture_6/file_organizer.py
+++ richardbenephraim/lecture_6/file_organizer.py
@@ -3,6 +3,8 @@
 import os
 import shutil
 import sys
+import subprocess
+
 
 
 def main():
@@ -20,15 +22,16 @@ def main():
 
 def program_main_logic():
     
-    checker = operating_system_checker()
+    
 
-    header = f"*"*10 + " FILES SORTING HANDLER " + "*"*10
+    header = f"*"*10 + " QuickFile Utility " + "*"*10
 
     message = input(
         f"\n{header}\n"
         "1: scan files \n"
         "2: Sort files in Download folder\n"
-        "\nselect option to continue or 'n' to exit program: \n"
+        "3: Youtube Downloader\n"
+        "select option to continue or 'n' to exit program: \n"
     ).strip().lower()
 
 
@@ -39,6 +42,8 @@ def program_main_logic():
             return file_folder_scanner()
         case "2":
             return sorting_operations_Downloads()
+        case "3":
+            return downloader_files()
         case _:
             return "invalid user choice! "
 
@@ -77,6 +82,124 @@ def file_folder_scanner():
                 print(file)
         case _:
             return "invalid user choice"
+
+
+
+
+def downloader_files():
+
+    check = operating_system_checker()
+    first_time_installer = input(
+        "\nFirst time installer: \n"
+        "[y]es or [n]o"
+        "type 'y' or 'n' to continue\n"
+    ).strip().lower()
+
+
+    match check:
+        case "Computer is a Debian OS(Ubuntu)":
+            if first_time_installer in ["yes", "y"]:
+                 working_with_debian_os()
+                 youtube_downloader()
+                 print()
+            else:
+                youtube_downloader()
+            
+
+           
+
+        case "Computer is Windows":
+            if first_time_installer in ["yes", "y"]:
+                working_with_windows_os
+                youtube_downloader
+            else:
+                youtube_downloader()
+           
+            
+
+
+
+
+
+
+def youtube_downloader():
+    import yt_dlp
+    url = input(" enter URL here: \n")
+    yt_options = {
+        'format': 'best',
+        'cookiefile': 'youtube_cookies.txt',
+         'nocheckcertificate': True
+    }
+    try:
+        with yt_dlp.YoutubeDL(yt_options) as stream:
+            stream.download(url_list=[url])
+            print("files downloaded successfully")
+    except Exception as e:
+        print(f"{e}...\n sorry DOWNLOAD FAILED ...")
+     
+        current_path = os.path.curdir
+        root_folder = os.path.expanduser("~/Downloads")
+        list_dir = os.listdir(current_path)
+        for downloaded_file in list_dir:
+            if downloaded_file.endswith((".mkv", ".mp4", ".avi", ".mov", ".wmv", ".flv", ".webm")):
+                shutil.move(src=f"{current_path}/{downloaded_file}", dst=root_folder)
+                print("downloaded file has been moved to Root Downloads Folder")
+            
+        print("no new video file downloaded")
+
+    
+
+
+
+    
+def working_with_debian_os():
+
+    print("Please wait while we update & upgrade US OS ....")
+    command = ["sudo", "apt", "update"]
+    command_2 = ["sudo","apt", "upgrade", "-y"]
+    subprocess.run(command)
+    subprocess.run(command_2)
+   
+   
+
+
+    ffmpeg =shutil.which("ffmpeg")
+    if ffmpeg == True:
+        print("ffmpeg is Found on Pc Skipping to next Dependencies .\n")
+    else:
+        subprocess.run(["sudo", "apt", "install", "ffmpeg"])
+        print("ffmpeg installed successfully")
+    
+
+
+    ytdlp = shutil.which("yt-dlp")
+    if ytdlp == True:
+        print("Proceeding to next ...\n")
+    else:
+        command_3 = ["sudo","add-apt-repository","https://github.com", "ppa:tomtomtom/yt-dlp"]
+        subprocess.run(command_3)
+        subprocess.run(["sudo", "apt", "update"])
+        subprocess.run(["sudo", "apt", "install", "yt-dlp"])
+
+    
+
+
+def working_with_windows_os():
+    ffmpeg =shutil.which("ffmpeg")
+    if ffmpeg == True:
+        print("ffmpeg is Found on Pc Skipping to next Dependencies .\n")
+    else:
+        print("installing ffmpeg .... .")
+        subprocess.run(["winget", "install", "ffmpeg"])
+        print("ffmpeg installed successfully")
+    
+    ytdlp = shutil.which("yt-dlp")
+    if ytdlp == True:
+        print("Proceeding to next ...\n")
+    else:
+        subprocess.run(["winget", "install", "yt-dlp"])
+
+
 
 
 
@@ -161,31 +284,31 @@ def sorting_operations_Downloads():
             if file.endswith((".mkv", ".mp4", ".avi", ".mov", ".wmv", ".flv", ".webm")):
                 # print(file, "Moved Successfully")
                 try:
-                    shutil.move(src=f"{path}/{file}", dst=f"/home/localcode-tech/Downloads/MOVIES_FILES")
+                    shutil.move(src=f"{path}/{file}", dst=f"{path}/MOVIES_FILES")
                 except (FileExistsError, FileNotFoundError) as e:
                     print(e)
             elif file.endswith((".mp3", ".wav", ".aac", ".ogg", ".flac", ".m4a")):
                 # print(file, "Moved Successfully")
                 try:
-                    shutil.move(src=f"{path}/{file}", dst=f"/home/localcode-tech/Downloads/MUSIC_FILES")
+                    shutil.move(src=f"{path}/{file}", dst=f"{path}/MUSIC_FILES")
                 except (FileExistsError, FileNotFoundError) as e:
                     print(e)
 
             elif  file.endswith((".zip", ".iso", ".msi", ".dmg", ".exe", ".rar", "7z")):
                 # print(file, "Moved Successfully")
                 try:
-                    shutil.move(src=f"{path}/{file}", dst=f"/home/localcode-tech/Downloads/SOFTWARE_FILES")
+                    shutil.move(src=f"{path}/{file}", dst=f"{path}/SOFTWARE_FILES")
                 except (FileExistsError, FileNotFoundError) as e:
                     print(e)
             elif file.endswith((".pdf", ".txt", ".csv", ".docx", ".xlsx", ".pptx", ".odt")):
                 # print(file, "move successfully")
                 try:
-                    shutil.move(src=f"{path}/{file}", dst=f"/home/localcode-tech/Downloads/DOCUMENT_FILES")
+                    shutil.move(src=f"{path}/{file}", dst=f"{path}/DOCUMENT_FILES")
                 except (FileExistsError, FileNotFoundError) as e:
                     print(e)
             elif file.endswith((".jpg", ".jpeg", ".png", ".gif", ".bmp", "webp", ".tiff", ".svg")):
                 try:
-                    shutil.move(src=f"{path}/{file}", dst=f"/home/localcode-tech/Downloads/DOCUMENT_FILES")
+                    shutil.move(src=f"{path}/{file}", dst=f"{path}/DOCUMENT_FILES")
                 except (FileExistsError, FileNotFoundError) as e:
                     print(e)
 
