8de9c6965de2736e1b8ec2d4c089891ab21ff5aa
localcodetech <rbphraim95@gmail.com>
2026-05-25 13:01:29 +0000

    for loop

---
+++ richardbenephraim/javascripts/main.js
@@ -0,0 +1,49 @@
+
+
+// // variables
+
+
+
+// // operators
+
+// const c = 4
+
+// console.log(c === "4")
+
+// const h = undefined;
+
+
+// let myDate = new Date()
+
+// console.log(myDate)
+
+
+// 2 == "2" ? console.log("hello") : console.log("bye")
+
+// const num = 14;
+// const num2 = 0;
+//  const cc = num2 || num;
+
+//  console.log(cc)
+
+const name = 45;
+
+
+typeof name === "string" ? console.log(" this is a string") : console.log("not")
+
+
+console.log(typeof name)
+
+
+// == CONDITIONS ==
+
+
+let school = "open labs";
+
+if (school.length >= 9) {
+    console.log("this number has 9 or more pictures")
+} else {
+console.log("charaters are less")
+};
+
+print()
