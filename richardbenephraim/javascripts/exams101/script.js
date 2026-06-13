066df9788404ee4997e34f842eca34bc2d224280
localcodetech <rbphraim95@gmail.com>
2026-06-13 00:34:50 +0000

    fix

--- richardbenephraim/javascripts/exams101/script.js
+++ richardbenephraim/javascripts/exams101/script.js
@@ -1,14 +1,11 @@
-
 // ADD EVENT LISTENER TO THE BUTTON
 
+const btn = document.getElementById("btn");
 
-const btn = document.getElementById('btn')
+btn.addEventListener("click", () => {
+  console.log(`Hello its me Ephraim`);
+});
 
+const sect = document.getElementById("sect");
 
-btn.addEventListener('click', ()=>{
-    console.log(`Hello its me Ephraim`)
-    
-})
-
-const sect = document.getElementById('sect')
-
+const datat = "heeJlo";
