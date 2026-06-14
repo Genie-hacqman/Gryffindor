361abf4e6a85dde7c46c7fd78ade9d70dfddca45
localcodetech <rbphraim95@gmail.com>
2026-06-14 00:49:53 +0000

    basic react

--- richardbenephraim/javascripts/document_object_model/main.js
+++ richardbenephraim/javascripts/document_object_model/main.js
@@ -1,1 +1,20 @@
-console.log(document.querySelector('.header'), document.querySelector('#hard').textContent)
+// console.log(document.querySelector('.header'), document.querySelector('#hard').textContent)
+
+const dd = document.querySelector('#hard')
+
+
+console.log(dd.textContent)
+
+const addd = document.createElement('p')
+
+dd.appendChild(addd)
+
+console.log(dd)
+
+
+const textin = document.textContent = 'this is a text'
+
+addd.textContent = textin
+
+
+addd.re
