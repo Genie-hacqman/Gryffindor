ac7b025e0d945b3d5aa6924222f982a0e87bf25f
localcodetech <rbphraim95@gmail.com>
2026-06-23 00:03:48 +0000

    learning

--- richardbenephraim/javascripts/task-manager/src/App.jsx
+++ richardbenephraim/javascripts/task-manager/src/App.jsx
@@ -1,4 +1,4 @@
-
+import TaskList from "./components/TaskList";
 
 
 
@@ -10,6 +10,7 @@ function App(){
       <h1>task manager</h1> 
       <p>this is a task manager {name}</p>
       <p>{45 > 5 ? 'yes its u' : 'no'}</p>
+      {/* <TaskList /> */}
     </div>
   )
 }
