22cd5abcd445be6eb0bfbdf7cee7e679103c84fb
localcodetech <rbphraim95@gmail.com>
2026-06-23 12:27:12 +0000

    react

---
+++ richardbenephraim/react/my-first-project/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+import back from './assets/backend-development-64.png'
+import React from 'react'
+
+
+function App(){
+  const station = 'LocalStreamGh';
+
+  return (
+    <React.Fragment>
+      <h1>welcome</h1>
+      <p>{station}</p>
+     
+
+    </React.Fragment>
+  )
+};
+
+export default App
