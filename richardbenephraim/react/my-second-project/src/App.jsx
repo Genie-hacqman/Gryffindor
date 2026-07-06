e85b0e971570762a412f29e6b4fab6e4508e8db6
localcodetech <rbphraim95@gmail.com>
2026-07-06 02:11:27 +0000

    fix

--- richardbenephraim/react/my-second-project/src/App.jsx
+++ richardbenephraim/react/my-second-project/src/App.jsx
@@ -3,7 +3,17 @@ import MapChannel from "./components/MapChannel"
 
 
 
+
 const App = ()=>{
+
+  const url = "https://localcodetech.pythonanywhere.com//api/channels"
+
+  async function getData() {
+
+    const data = await fetch(url).then((res)=>res.json)
+    
+  }
+
   return(
     <div>
 
