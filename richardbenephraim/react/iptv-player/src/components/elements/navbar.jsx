fe12ce75d8af342b318f828204f02dc177bf4474
localcodetech <rbphraim95@gmail.com>
2026-07-16 06:00:49 +0000

    iptv

--- richardbenephraim/react/iptv-player/src/components/elements/navbar.jsx
+++ richardbenephraim/react/iptv-player/src/components/elements/navbar.jsx
@@ -1,5 +1,6 @@
 import Logo from "../ui/logo"
 import image1 from "../../assets/favicon.png"
+import Navlinks from "./navlinks"
 
 function Navbar() {
   return (
@@ -12,11 +13,16 @@ function Navbar() {
     
         <div>
           {/* navlinks */}
-          
+          < Navlinks link = "/" text={"home"}/>
+          < Navlinks link = "/movies" text={"movies"}/>
+          < Navlinks link = "/tvshows" text={"tv shows"}/>
+          < Navlinks link = "/mylist" text={"mylist"}/>
+          < Navlinks link = "/support" text={"support"}/>
         </div>
 
         <div>
           {/* call to action */}
+
 
         </div>
       </section>
